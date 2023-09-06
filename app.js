66790df2038037d58a1958ff9ca8ab79925132f3
Leonardo Cuevas <leocuevas73@gmail.com>
2023-09-06 18:32:25 -0300

    dev routes, minor fixes

--- app.js
+++ app.js
@@ -11,15 +11,12 @@ app.use(cookieParser());
 import userRoutes from './src/routes/user_routes.js';
 import spreadsheetRoutes from './src/routes/spreadsheet_routes.js';
 import itemRoutes from './src/routes/item_routes.js';
-
-// app.use((req, res, next) => {
-// 	if (req.method !== 'POST') return res.status(405).send('Not allowed.');
-// 	next();
-// });
+// import devRoutes from './src/routes/dev_routes.js';
 
 app.use('/api/user', userRoutes);
 app.use('/api/spreadsheet', spreadsheetRoutes);
 app.use('/api/item', itemRoutes);
+// app.use('/api/dev', devRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -4,8 +4,6 @@ import validateForm from '../middleware/validateForm.js';
 import { verifyAuthorization } from '../middleware/verifyAuthorization.js';
 import user from '../controllers/user_controllers.js';
 
-import db from '../modules/mongodb.js';
-
 router.post('/login', validateForm.login, user.login);
 router.post('/forgot-password', user.forgotPassword);
 router.post('/new-password', user.forgotPassword);
@@ -19,60 +17,5 @@ router.post('/modify', user.modify);
 router.post('/allusers', user.allUsers);
 router.post('/refresh-session', user.refreshSession);
 router.post('/logout', user.logout);
-
-// import notifyUsers from '../modules/gmail.js'
-
-// router.get('/testmail', async (req, res) => {
-// 	let changeData = {
-// 		_id: {
-// 			$oid: '622f5f0c876d2f5579da2682',
-// 		},
-// 		section: 'Monitoring',
-// 		changelog: [
-// 			{
-// 				date: '14/12/2023',
-// 				time: '13:24:48',
-// 				details: ['Creación'],
-// 				user: 'Cuevas Leonardo',
-// 			},
-// 			{
-// 				date: '28/12/2023',
-// 				time: '15:44:48',
-// 				details: ["Estado: 'Solicitado' --> 'Aprobado'", "Estado: 'Solicitado' --> 'Aprobado'"],
-// 				user: 'Machado Emilce',
-// 			},
-// 		],
-// 		startDate: '14/12/2023',
-// 		coverData: {
-// 			name: 'Cuevas Leonardo',
-// 			date: '17/12/2023',
-// 			shift: '14 a 22 hs.',
-// 			day: 'Lunes',
-// 			guardId: 'A',
-// 		},
-// 		returnData: {
-// 			name: 'Conti Mauro',
-// 			date: '23/12/2023',
-// 			day: 'Domingo',
-// 			shift: '22 a 6 hs.',
-// 			guardId: 'E',
-// 		},
-// 		status: 'Autorizado',
-// 		__v: 0,
-// 	};
-
-// 	try {
-// 		let mailId = await notifyUsers(
-// 			changeData,
-// 			'Aprobado',
-// 			`Su cambio de guardia ha sido aprobado`,
-// 			'Monitoring',
-// 			'change',
-// 		);
-// 		res.status(200).json({ mailId });
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// });
 
 export default router;
